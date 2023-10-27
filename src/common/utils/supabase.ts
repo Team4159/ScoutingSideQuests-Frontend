4a038a8ae7521e34571a42e7b8c4575713a4b93b
Calvin Lu <calvinlu@live.com>
2023-10-26 23:40:47 -0700

    temp fix @utils/supabase.ts

--- src/common/utils/supabase.ts
+++ src/common/utils/supabase.ts
@@ -5,3 +5,5 @@ const supabase = createClient<Database>(
     "zcckkiwosxzupxblocff",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjY2traXdvc3h6dXB4YmxvY2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NDk3MzMsImV4cCI6MjAwMjQyNTczM30.IVIT9yIxQ9JiwbDB6v10ZI8eP7c1oQhwoWZejoODllQ"
 );
+
+export default supabase
