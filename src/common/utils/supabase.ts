8ac5e6c34f5dd26c8042961a80976cf172f80e7e
Calvin Lu <calvinlu@live.com>
2023-10-27 16:40:38 -0700

    Update supabase utils

--- src/common/utils/supabase.ts
+++ src/common/utils/supabase.ts
@@ -6,4 +6,29 @@ const supabase = createClient<Database>(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjY2traXdvc3h6dXB4YmxvY2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NDk3MzMsImV4cCI6MjAwMjQyNTczM30.IVIT9yIxQ9JiwbDB6v10ZI8eP7c1oQhwoWZejoODllQ"
 );
 
-export default supabase;
+const getAllAchievementNames = async (): Promise<string[] | null> => {
+    const res = await supabase.from("achievements").select("name");
+    
+    if (res.data === null) {
+        return null;
+    }
+
+    return res.data.map((achievement) => achievement.name);
+}
+
+const addAchievement = async (user: string, achievementName: string, description?: string): Promise<boolean> => {
+    const res = await supabase.from("achievement_queue").insert({
+        achievement: achievementName,
+        description: description,
+        season: process.env.SEASON,
+        event: process.env.EVENT,
+        user: user,
+    });
+    
+    return res.status < 300;
+}
+
+export {
+    getAllAchievementNames,
+    addAchievement,
+}
