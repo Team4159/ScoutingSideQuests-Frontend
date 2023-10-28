4f8f00e94a9a3c54392583234be3bcccf4a73af0
Sebastian Dowell <KN6BDL@protonmail.com>
2023-10-27 17:29:01 -0700

    improved admin panel css and structure (albeit not complete)

--- src/admin/admin.tsx
+++ src/admin/admin.tsx
@@ -1,20 +1,21 @@
-import "./admin.css"
+import "./admin.css";
 
 const Admin = () => {
     return (
         <div className="admin-container">
             <div className="tinder-container">
-                <button className="accept-button">Accept</button>
                 <div className="info-card">
                     <h3>Image:</h3>
-                    <img src="https://placekitten.com/300/300"></img>
+                    <img src="https://placekitten.com/300/300" alt="kitten"></img>
                     <h3>Text:</h3>
                     <p>Placeholder text yayayayayayay lorem ipsum yippee i love side quests</p>
+                    <div className="action-buttons">
+                        <button className="accept-button">Accept</button>
+                        <button className="reject-button">Reject</button>
+                        <button className="undo-button">Undo Last</button>
+                    </div>
                 </div>
-                <button className="reject-button">Reject</button>
             </div>
-            
-            <button className="undo-button">Undo Last</button>
         </div>
     );
 };
