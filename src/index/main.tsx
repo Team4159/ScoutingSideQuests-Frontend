20296a640ef1d59b2259d1b284c3342fdf5bd1e3
Sebastian Dowell <sebastiandowell@Sebastians-MacBook-Air.local>
2023-11-20 02:28:34 -0800

    improved css, password fetch & cookie logic

--- src/index/main.tsx
+++ src/index/main.tsx
@@ -1,6 +1,6 @@
 import "./main.css";
 
-const Main = () => {
+const Main = () => { 
     return (
         <div className="container">
             <div className="box">
@@ -9,6 +9,7 @@ const Main = () => {
                         <h3>Team 4159 Side Quests</h3>
                         <p>Use your hours password to sign in.</p>
                     </div>
+                    <br></br>
                     <br></br>
                     <div className="input-group">
                         <Input />
@@ -30,12 +31,39 @@ const Input = () => {
 }
 
 const Submit = () => {
+    const doesCookieExist = (name: string) => {
+        const cookies = document.cookie.split(';').map(cookie => cookie.trim());
+        return cookies.some(cookie => cookie.startsWith(`${name}=`));
+    };
+
+    const handleData = (data: any) => {
+        if (!doesCookieExist('name')) {
+            document.cookie = `name=${data.name}; path=/`;
+        } else {
+            console.log("Cookie already exists"); // debug
+        }
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const passwordInput = document.getElementById('password') as HTMLInputElement;
+        const password = passwordInput.value;
+        const apiUrl = `https://hours.team4159.org/users/getuserdata?password=${password}`;
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => handleData(data))
+            .catch((error) => console.error('Error:', error));
+        passwordInput.value = '';
+    };
+
     return (
-        <div className="submit">
-            <div className="input">
-                <input type="submit" className="input-submit" value="Sign In" required></input>
+        <form onSubmit={handleSubmit}>
+            <div className="submit">
+                <div className="input">
+                    <input type="submit" className="input-submit" value="Sign In" required></input>
+                </div>
             </div>
-        </div>
+        </form>
     );
 }
 
